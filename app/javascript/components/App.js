60551e2697a9656e4bd6481e4a6a3ad285f9655b
Chris Kheng <chriskheng1609@gmail.com>
2019-12-25 16:13:45 +0800

    Add React-BootStrap and do basic styling to homepage

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,15 @@ import React from "react"
 import PropTypes from "prop-types"
 import Person from "./Person"
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+
 class App extends React.Component {
   state = { value: 0 }
 
@@ -11,10 +20,35 @@ class App extends React.Component {
   }
 
   render () {
+      const NavBar = <Navbar variant="dark"  bg="dark" expand="lg">
+          <Navbar.Brand href="#home">AlphaBlog</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#link">Link</Nav.Link>
+                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                      <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                  </NavDropdown>
+              </Nav>
+              <Form inline>
+                  <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                  <Button variant="outline-success">Search</Button>
+              </Form>
+          </Navbar.Collapse>
+      </Navbar>
+
     return (
       <React.Fragment>
-        <h1>Homepage</h1>
-        <p>Yep this is working!</p>
+          {NavBar}
+        <Jumbotron>
+            <h1>Homepage</h1>
+            <p>Yep this is working!</p>
+        </Jumbotron>
         <Person value = {this.state.value}></Person>
         <button onClick={this.switchNameHandler}>Hit it</button>
       </React.Fragment>
